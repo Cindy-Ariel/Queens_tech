00ba6539be7cbea245d6c2131c5ac28473e51054
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-08-12 12:53:18 -0300

    teste

---
+++ src/controllers/cliente-controllers.js
@@ -0,0 +1,151 @@
+
+import ClienteModel from "../model/cliente-model.js";
+
+import ClienteDAO from "../dao/cliente-DAO.js";
+
+
+//import { validAll as validacoesServices } from "../services/validacao.js";
+
+const clienteController = (app, bd) => {
+  const clienteDAO = new ClienteDAO(bd);
+
+  app.get("/cliente", async (req, res) => {
+    try {
+      res.json(await clienteDAO.listaClientes());
+    } catch (error) {
+      res.status(400).json({
+        msg: error.message,
+        error: true,
+      });
+    }
+  });
+
+  app.get("/cliente/id/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+      //await clienteDAO._verificaId(id);
+      const client = await clienteDAO.pegaUmCliente(id);
+      res.status(201).json(client);
+    } catch (error) {
+      res.status(404).json({
+        msg: error.message,
+        error: true,
+      });
+    }
+  });
+
+  app.get("/cliente/email/:email", async (req, res) => {
+    const email = req.params.email;
+    try {
+      await clienteDAO._verificaEmail(email);
+      const client = await clienteDAO.pegaUmClienteporEmail(email);
+      res.status(201).json(client);
+    } catch (error) {
+      res.status(404).json({
+        msg: error.message,
+        error: true,
+      });
+    }
+  });
+
+  app.post("/cliente/login", async (req, res) => {
+    const { EMAIL, SENHA } = req.body;
+    try {
+      const login = await clienteDAO.pegaUmClienteporEmail(EMAIL);
+      if (EMAIL !== login.usuario.EMAIL || SENHA !== login.usuario.SENHA) {
+        return res.status(400).json({
+          message: "Email ou senha inválidas!",
+          error: true,
+        });
+      }
+      res.status(200).json({
+        usuario: login.usuario,
+        msg: `Usuario ${login.usuario.NOME_COMPLETO} logado!`,
+      });
+    } catch (error) {
+      res.status(400).json({
+        message: "Email ou senha inválidas!",
+        error: true,
+      });
+    }
+  });
+
+  app.post("/cliente", async (req, res) => {
+    const body = req.body;
+    try {
+        const novoCliente = new ClienteModel(
+            body.CPF,
+            body.NOME,
+            body.EMAIL,
+            body.TELEFONE,
+            body.RUA,
+            body.NUMERO,
+            body.BAIRRO,
+            body.CIDADE,
+            body.UF,
+            body.CEP,
+            body.SENHA
+        );
+        console.log(novoCliente)
+        res.status(201).json(await clienteDAO.insereCliente(novoCliente));
+      } catch (error) {
+      res.status(400).json({
+        msg: error.message,
+        error: true,
+      });
+    }
+  });
+
+  app.delete("/cliente/id/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+      await clienteDAO._verificaId(id);
+      const delUsuario = await clienteDAO.deletaCliente(id);
+      res.status(201).json(delUsuario);
+    } catch (error) {
+      res.status(400).json({
+        msg: error.message,
+        error: true,
+      });
+    }
+  });
+
+  app.put("/cliente/id/:id", async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+
+    try {
+      if (
+        validacoesServices(
+          body.NOME_COMPLETO,
+          body.CPF,
+          body.TELEFONE,
+          body.SENHA,
+          body.EMAIL
+        )
+      ) {
+        const clienteAtualizado = new ClienteModel(
+          body.ID,
+          body.NOME_COMPLETO,
+          body.CPF,
+          body.TELEFONE,
+          body.SENHA,
+          body.EMAIL
+        );
+        await clienteDAO._verificaId(id);
+        const attCliente = await clienteDAO.atualizaCliente(
+          id,
+          clienteAtualizado
+        );
+        res.status(200).json(attCliente);
+      }
+    } catch (error) {
+      res.status(400).json({
+        msg: error.message,
+        error: true,
+      });
+    }
+  });
+};
+
+export default clienteController;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import clienteController from "./controllers/cliente-controllers.js";
 import bd from "./infra/sqlite-db.js";
 const app = express();
 
 app.use(express.json());
 
+clienteController(app,bd)
 
 
 app.listen(3000);
