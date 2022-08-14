ec385b192ba156bb9a885e738e160e8c8f9b9e45
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-08-14 18:42:57 -0300

    ✨ feat: Criação do Model do Produto

---
+++ src/model/produto-model.js
@@ -0,0 +1,17 @@
+class ProdutoModel {
+
+
+    constructor(nome, categoria, marca, cor, valor,descricao, fornecedor) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.marca = marca;
+        this.cor = cor;
+        this.valor = valor;
+        this.descricao = descricao;
+        this.fornecedor = fornecedor;
+
+
+    }
+}
+
+export default ProdutoModel
