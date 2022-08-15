5d8fdff61e0e5343e2f2fe0b8fc69a54a99cccfa
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-08-15 08:38:47 -0300

    ✨ feat: Importando ProdutoController no server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import clienteController from "./controllers/cliente-controllers.js";
+import ProdutoController from "./controllers/produto-controller.js"
+
 import bd from "./infra/sqlite-db.js";
 const app = express();
 
 app.use(express.json());
 
 clienteController(app,bd)
+ProdutoController(app,bd)
 
 
 app.listen(3000);
