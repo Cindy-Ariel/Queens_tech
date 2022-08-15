94308afcd7d858ec81913d53fb6ce80941154bbe
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-08-15 09:18:26 -0300

    ✨ feat: Importando o FornecedorController no server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import clienteController from "./controllers/cliente-controllers.js";
 import ProdutoController from "./controllers/produto-controller.js"
-
+import FornecedorController from "./controllers/fornecedor-controller.js";
 import bd from "./infra/sqlite-db.js";
 const app = express();
 
@@ -9,6 +9,7 @@ app.use(express.json());
 
 clienteController(app,bd)
 ProdutoController(app,bd)
+FornecedorController(app,bd)
 
 
 app.listen(3000);
