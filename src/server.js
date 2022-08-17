6321ddcf6961fb9abc289bb252c5e0c0ff860233
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-08-16 21:56:04 -0300

    ♻️ Alimentando validações

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import clienteController from "./controllers/cliente-controllers.js";
+import vendasController from "./controllers/vendas-controllers.js";
 import bd from "./infra/sqlite-db.js";
 const app = express();
 
 app.use(express.json());
 
-clienteController(app,bd)
+clienteController(app,bd);
+vendasController(app,bd)
 
 
 app.listen(3000);
