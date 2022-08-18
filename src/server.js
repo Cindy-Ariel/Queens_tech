e740b433a258a26d8647b999327ffd0228865704
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-08-18 12:17:04 -0300

    Importando arquivo EstoqueController

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import clienteController from "./controllers/cliente-controllers.js";
 import ProdutoController from "./controllers/produto-controller.js"
 import FornecedorController from "./controllers/fornecedor-controller.js";
 import vendasController from "./controllers/vendas-controllers.js";
+import EstoqueController from "./controllers/estoque-controller.js"
 
 import bd from "./infra/sqlite-db.js";
 import cors from "cors"
@@ -16,10 +17,11 @@ const corsConfig = {
 
 app.use(cors(corsConfig));
 
-clienteController(app,bd)
-ProdutoController(app,bd)
-FornecedorController(app,bd)
-vendasController(app,bd)
+clienteController(app,bd);
+ProdutoController(app,bd);
+FornecedorController(app,bd);
+vendasController(app,bd);
+EstoqueController(app,bd);
 
 const port = process.env.PORT || 3000;
 
