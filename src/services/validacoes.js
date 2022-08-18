067ff3c0c730ded84abf84e91a3bdb137f00071a
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-08-18 12:31:44 -0300

    Revertendo alterações. Inserindo validações do funcionário

--- src/services/validacoes.js
+++ src/services/validacoes.js
@@ -70,5 +70,19 @@ export const validaEntradaEstoque = async (estoque) => {
 
 }
 
+// VALIDANDO FUNCIONARIO
+export const validaExistenciaDeFuncionario = (funcionario, res) => {
+    if (funcionario) {
+        return res.status(201).json(funcionario);
+    } else {
+        throw new Error("Não há um funcionario com a informação inserida")
+    }
+};
 
-// export { verificaSeExisteObjeto, verificaCampoVazio, validaEntradaCliente }
+export const validaFuncionario = (nome, cpf, rg, cargo, telefone, rua, numero, bairro, cidade, uf, cep, cnpj) => {
+    if (nome && cpf && rg && cargo && telefone && rua && numero && bairro && cidade && uf && cep && cnpj) {
+        return true;
+    } else {
+        throw new Error("Alguma informação não foi inserida.");
+    }
+};
